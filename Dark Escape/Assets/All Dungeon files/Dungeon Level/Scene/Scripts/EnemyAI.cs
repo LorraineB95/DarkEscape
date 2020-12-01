50e78504c5aa0c0859399adf1b6a58bd6a124c1e
LorraineB95 <72505972+LorraineB95@users.noreply.github.com>
2020-12-01 14:53:48 +0000

    Dark Escape Project

---
+++ Dark Escape/Assets/All Dungeon files/Dungeon Level/Scene/Scripts/EnemyAI.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    NavMeshAgent nm;
+    public Transform target;
+
+    public float distanceThreshold = 10f;
+
+    public enum AIState { idle,chasing};
+    public AIState aiState = AIState.idle;
+
+    public Animator animator;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nm = GetComponent<NavMeshAgent>();
+        StartCoroutine(Think());
+        //new if doesn't work then delete
+        target = GameObject.FindGameObjectWithTag("Player").transform; 
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Think()
+    {
+        while(true)
+        {
+            switch (aiState)
+            {
+                case AIState.idle:
+                    float dist = Vector3.Distance(target.position, transform.position);
+                    if(dist < distanceThreshold)
+                    {
+                        aiState = AIState.chasing;
+                        animator.SetBool("Chasing", true);
+                        
+                    }
+                    nm.SetDestination(transform.position);
+                    break;                  
+                case AIState.chasing:
+                    dist = Vector3.Distance(target.position, transform.position);
+                    if (dist > distanceThreshold)
+                    {
+                        aiState = AIState.idle;
+                        animator.SetBool("Chasing", false);
+                        
+                    }
+                    nm.SetDestination(target.position);
+                    break;
+                default:
+                    break;
+            }
+            
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
---
+++ Dark Escape/Assets/All Dungeon files/Dungeon Level/Scene/Scripts/Player.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    CharacterController cc;
+    public float speed = 10f;
+
+    //gravity
+    float ySpeed = 0f;
+    float gravity = -15f;
+    public Transform fpsCamera;
+    float pitch = 0f;
+
+    [Range(5, 15)]
+    public float mouseSensitivity = 10f;
+
+    [Range(45, 85)]
+    public  float pitchRange = 45f;
+
+    //member input values
+    float xInput = 0f;
+    float zInput = 0f;
+    float xMouse = 0f;
+    float yMouse = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+
+        UpdateMovement();
+        if(cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false)
+        {
+            GetComponent<AudioSource>().volume = Random.Range(0.8f, 1);
+            GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+    void GetInput()
+    {
+        xInput = Input.GetAxis("Horizontal") * speed;
+        zInput = Input.GetAxis("Vertical") * speed;
+        xMouse = Input.GetAxis("Mouse X") * mouseSensitivity;
+        yMouse = Input.GetAxis("Mouse Y") * mouseSensitivity;
+    }
+    void UpdateMovement()
+    {
+        Vector3 move = new Vector3(xInput, 0, zInput);
+        move = Vector3.ClampMagnitude(move, speed);
+        move = transform.TransformVector(move);
+
+        if (cc.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                ySpeed = 15f;
+            }
+            else
+            {
+                ySpeed = gravity * Time.deltaTime;
+            }
+        }
+        else
+        {
+            ySpeed += gravity * Time.deltaTime;
+        }
+
+        //Actually apply the movement
+        cc.Move((move + new Vector3(0, ySpeed, 0)) * Time.deltaTime);
+        transform.Rotate(0, xMouse, 0);
+        pitch -= yMouse;
+        pitch = Mathf.Clamp(pitch, -pitchRange, pitchRange);
+        Quaternion camRotation = Quaternion.Euler(pitch, 0, 0);
+        fpsCamera.localRotation = camRotation;
+    }
+
+}//End of player
